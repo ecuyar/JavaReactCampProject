576e34b7ca9f39cfbda7075bf446b663df795158
Enes Can UYAR <uyar.enescan@gmail.com>
2021-06-02 09:00:17 +0300

    Entity database relation

--- src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -1,10 +1,14 @@
 package kodlamaio.northwind.entities.concretes;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -17,14 +21,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "product_id")
 	private int id;
 
-	@Column(name = "category_id")
-	private int categoryId;
+//	@Column(name = "category_id")
+//	private int categoryId;
 
 	@Column(name = "product_name")
 	private String productName;
@@ -37,4 +41,8 @@ public class Product {
 
 	@Column(name = "quantity_per_unit")
 	private String quantityPerUnit;
+
+	@ManyToOne()
+	@JoinColumn(name = "category_id")
+ 	private Category category;
 }

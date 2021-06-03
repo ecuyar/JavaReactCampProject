93d32c5e83a518ea326d7a9782c43f5da915d3a2
Enes Can UYAR <uyar.enescan@gmail.com>
2021-06-03 09:33:36 +0300

    dao > productservice > productmanager

--- src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -8,7 +8,25 @@ import kodlamaio.northwind.entities.concretes.Product;
 
 public interface ProductService {
 
+	// dataresult ya da result dönecek her zaman
 	DataResult<List<Product>> getAll();
 
 	Result add(Product product);
+
+	// productdao dan aldıklarımızı buraya ekliyoruz sonrada dataresult tipine
+	// çeviriyoruz. Dao'ya eklediğimiz metotlar servise eklenince business classtaki
+	// productmanagerda çağrılabilir hale geliyor
+	DataResult<Product> getByProductName(String productName);
+
+	DataResult<Product> getByProductNameAndCategoryId(String productName, int id);
+
+	DataResult<List<Product>> getByProductNameOrCategoryId(String productName, int id);
+
+	DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories);
+
+	DataResult<List<Product>> getByProductNameContains(String productName);
+
+	DataResult<List<Product>> getByProductNameStartsWith(String productName);
+
+	DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId);
 }

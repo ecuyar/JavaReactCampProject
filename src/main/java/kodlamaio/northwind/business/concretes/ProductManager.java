0cdff2f3ef99f76becd4671236743ef30e8631b8
Enes Can UYAR <uyar.enescan@gmail.com>
2021-06-07 00:23:54 +0300

    Fixed minor errors, added extra methods-2

--- src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -32,6 +32,20 @@ public class ProductManager implements ProductService {
 		return new SuccessDataResult<List<Product>>((this.productDao.findAll()), "Data listelendi.");
 	}
 
+	// pageable ile çalışırken veriye .getContent eklenir
+	@Override
+	public DataResult<List<Product>> getAll(int pageNo, int pageSize) {
+		Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+		return new SuccessDataResult<List<Product>>((this.productDao.findAll(pageable).getContent()),
+				"Data listelendi.");
+	}
+
+	@Override
+	public DataResult<List<Product>> getAllSorted() {
+		Sort sort = Sort.by(Sort.Direction.DESC, "productName");
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(sort));
+	}
+
 	@Override
 	public Result add(Product product) {
 		this.productDao.save(product);
@@ -77,19 +91,5 @@ public class ProductManager implements ProductService {
 	public DataResult<List<Product>> getByNameAndCategoryId(String productName, int categoryId) {
 		return new SuccessDataResult<List<Product>>(
 				(this.productDao.getByNameAndCategory_CategoryId(productName, categoryId)), "Data listelendi.");
-	}
-
-	// pageable ile çalışırken veriye .getContent eklenir
-	@Override
-	public DataResult<List<Product>> getAll(int pageNo, int pageSize) {
-		Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
-		return new SuccessDataResult<List<Product>>((this.productDao.findAll(pageable).getContent()),
-				"Data listelendi.");
-	}
-
-	@Override
-	public DataResult<List<Product>> getAllSorted() {
-		Sort sort = Sort.by(Sort.Direction.DESC, "productName"); 
-		return new SuccessDataResult<List<Product>>(this.productDao.findAll(sort));
 	}
 }
